833b121fb53e379d58c5eb45e036c7ad2da9e06c
cbhallamudi <cbhallamudi@gmail.com>
2023-06-01 12:08:44 +0530

    completed till in-house build

--- assets/js/custom_scripts.js
+++ assets/js/custom_scripts.js
@@ -32,5 +32,23 @@ $('.owl-carousel').owlCarousel({
         }
     },
     navText:['<i class="fa-solid fa-caret-left"></i>','<i class="fa-solid fa-caret-right"></i>']
+});
+
+$('#loop').owlCarousel({
+    center: true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
 })
 
